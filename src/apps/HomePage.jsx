fae9f3e01121e413020688b4ab884251e9f941ca
akashksingh16 <akash.singh@kapturecrm.com>
2025-04-17 18:43:54 +0530

    added bg

--- src/apps/HomePage.jsx
+++ src/apps/HomePage.jsx
@@ -7,7 +7,7 @@ export const HomePage = () => {
   const path = location?.pathname?.replace("/", "");
 
   let bgClass = "bg-default";
-  if (path === "payment-approval") bgClass = "bg-b";
+  if (path === "invoice-uploader") bgClass = "bg-b";
 
   return <div className={`homepage  ${bgClass} `}></div>;
 };
